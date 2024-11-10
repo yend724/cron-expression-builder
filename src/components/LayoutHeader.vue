e7745e14acece208db4888660cb46e7550fd9583
yend724 <yend9524@gmail.com>
2024-11-10 22:01:19 +0900

    text

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="grid grid-cols-[1fr_auto] gap-x-2">
-    <h1 class="font-semibold">GitHub Actions Cron Expression Builder</h1>
+    <h1 class="font-semibold">Cron Expression Builder</h1>
   </header>
 </template>
